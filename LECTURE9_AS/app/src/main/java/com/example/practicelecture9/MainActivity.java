29e03b82ca82662e2542cfb2c8ad414c776d05c8
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-03-25 14:27:05 +0500

    Lecture9 practice-2

--- LECTURE9_AS/app/src/main/java/com/example/practicelecture9/MainActivity.java
+++ LECTURE9_AS/app/src/main/java/com/example/practicelecture9/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.practicelecture9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    SeekBar seekbar;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekbar= findViewById(R.id.seekBar);
+        listView= findViewById(R.id.ListViewSeekbar);
+        seekbar.setMax(20);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                //Log.i("Seek bar Value",String.valueOf(progress));
+                //int tablenumber =progress;
+                ArrayList<String> tablearraylist =new ArrayList<String>();
+                for (int x=1; x<51; x++)
+                {
+                    tablearraylist.add(Integer.toString(x*progress));
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,tablearraylist);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
